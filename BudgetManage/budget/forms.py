e1a1c3c2b940ca74bf6cddc3ca94ffd26d61b1db
viettrung2103 <viettrung21@gmail.com>
2021-11-26 11:32:01 +0200

    make method callable

--- BudgetManage/budget/forms.py
+++ BudgetManage/budget/forms.py
@@ -15,7 +15,7 @@ class MethodForm(ModelForm):
 
     #Force the first letter of the name to be capitalized
     def clean_name(self):
-        initial = self.cleaned_data('name')
+        initial = self.cleaned_data['name']
         if initial == '':
             raise ValidationError("You cannot add empty method")
         return initial.capitalize()
